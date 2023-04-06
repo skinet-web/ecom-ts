e49545a29d3664b5767fbd273e7246c50dc54df2
skinet-web <skinet2009@gmail.com>
2023-04-06 23:40:48 +0300

    Added mobile style

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -20,17 +20,21 @@ interface HeroBannerProps {
 
 const HeroBanner:FunctionComponent<HeroBannerProps> = ({heroData}) => {
   return (
-    <div className="flex mx-10 bg-gray-400">
-      <div className="flex flex-col justify-start items-start ml-4 ">
-        <h3>{heroData.smallText}</h3>
-        <h3>{heroData.midText}</h3>
-        <p>{heroData.largeText2}</p>
-        <Image src={urlFor(heroData.image).url()} width={500} height={500} />
-        
-        <button className="bg-red-500 text-white font-semibold p-3 
-        ml-2 rounded-full">
-          {heroData.buttonText}
+    <div className="flex mx-10 bg-gray-400 rounded-xl">
+      <div className="flex flex-col justify-start items-start ml-4 my-2">
+        <h3 className="font-semibold text-xl">{heroData.smallText}</h3>
+        <h3 className="font-bold text-5xl">{heroData.midText}</h3>
+        <p  className="text-white text-6xl uppercase font-semibold">{heroData.largeText2}</p>
+        <Image src={urlFor(heroData.image).url()} alt='headphones' width={500} height={500} />
+        <button className="bg-red-500 text-white font-semibold p-3 my-6 
+          ml-2 rounded-full ">
+            {heroData.buttonText}
         </button>
+        <div className="w-[95%] mb-3">
+          <h3 className="font-bold text-lg">Description</h3>
+          <p className="text-justify">{heroData.desc}</p>
+        </div>
+        
       </div>
     </div>
   );
