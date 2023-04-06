3c8b2b41128e115b3d36cf889a07ff3ee75abc31
skinet-web <skinet2009@gmail.com>
2023-04-06 23:01:30 +0300

    Added banner

--- app/components/HeroBanner.tsx
+++ app/components/HeroBanner.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { FunctionComponent } from "react";
+import Image from "next/image";
+import { urlFor } from "@/lib/client";
 
 interface HeroData {
+  image: String;
   smallText: string;
+  midText: string;
+  largeText1: string;
+  largeText2: string;
+  buttonText: string;
+  desc: string;
 }
 
 interface HeroBannerProps {
@@ -12,11 +20,17 @@ interface HeroBannerProps {
 
 const HeroBanner:FunctionComponent<HeroBannerProps> = ({heroData}) => {
   return (
-    <div>
-      <div>
+    <div className="flex mx-10 bg-gray-400">
+      <div className="flex flex-col justify-start items-start ml-4 ">
+        <h3>{heroData.smallText}</h3>
+        <h3>{heroData.midText}</h3>
+        <p>{heroData.largeText2}</p>
+        <Image src={urlFor(heroData.image).url()} width={500} height={500} />
         
-        <p>{heroData.smallText}</p>
-        <h3>MID TEXT</h3>
+        <button className="bg-red-500 text-white font-semibold p-3 
+        ml-2 rounded-full">
+          {heroData.buttonText}
+        </button>
       </div>
     </div>
   );
