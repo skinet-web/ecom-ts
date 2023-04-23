f01a4d7a13bb7c39e3d51fb1c099519de5626a0c
skinet-web <skinet2009@gmail.com>
2023-04-23 16:27:28 +0300

    .

--- app/product/[slug]/layout.tsx
+++ app/product/[slug]/layout.tsx
@@ -2,3 +2,10 @@ export const metadata = {
     title: 'Ecommerce - TS/Sanity',
     description: 'Ecommerce - TS',
   }
+
+
+  export default function layout({ children }: {
+    children: React.ReactNode
+  }) {
+    return <div>{children}</div>;
+  }
