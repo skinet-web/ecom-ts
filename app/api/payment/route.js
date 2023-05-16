29244bf8e16296c882c58b2568b46ed2cdc4ab4d
skinet-web <skinet2009@gmail.com>
2023-05-17 00:30:02 +0300

    Removed photo from checkout

--- app/api/payment/route.js
+++ app/api/payment/route.js
@@ -9,17 +9,17 @@ export async function POST(request) {
   let body = data.body;
   const session = await stripe.checkout.sessions.create({
     line_items: body.map((item) => {
-      const img = item.image[0].asset._ref;
-      const newImage = img
-        .replace("image-", "https://cdn.sanity.io/images/vfxfwnaw/production/")
-        .replace("-webp", ".webp");
+      // const img = item.image[0].asset._ref;
+      // const newImage = img
+      //   .replace("image-", "https://cdn.sanity.io/images/vfxfwnaw/production/")
+      //   .replace("-webp", ".webp");
 
       return {
         price_data: {
           currency: "RON",
           product_data: {
             name: item.name,
-            images: [newImage],
+            // images: [newImage],
           },
           unit_amount: item.price * 100,
         },
