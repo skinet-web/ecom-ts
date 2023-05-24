0d6732a5af49af3803e8a732b885d8ec6acce6f0
skinet-web <skinet2009@gmail.com>
2023-05-24 21:49:22 +0300

    Added Auth with github

--- app/components/loginPage.tsx
+++ app/components/loginPage.tsx
@@ -6,7 +6,7 @@ import { signIn, useSession } from "next-auth/react";
 const loginPage = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-5">
-      Login Page.
+      Login Page
       <button 
       type="button" 
       className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
