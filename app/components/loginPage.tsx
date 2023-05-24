76f25b8c5d58ebea0d2371d6386f7b7e3e28b2f6
skinet-web <skinet2009@gmail.com>
2023-05-24 21:42:17 +0300

    Added Auth with github

--- app/components/loginPage.tsx
+++ app/components/loginPage.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import React from "react";
-import GithubButton from 'react-github-login-button'
 import { signIn, useSession } from "next-auth/react";
 
 const loginPage = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center gap-5">
       Login Page.
-      <GithubButton onClick={() => signIn("github")} />
+      <button 
+      type="button" 
+      className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      onClick={() => signIn("github")}>
+        GitHub login
+      </button>
+      
     </div>
   );
 };
