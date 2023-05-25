ca353ab3d93b7b7f75269f7211dd94e2a0c9be82
skinet-web <skinet2009@gmail.com>
2023-05-25 21:47:33 +0300

    Added toast with login confirmation

--- app/components/loginPage.tsx
+++ app/components/loginPage.tsx
@@ -2,15 +2,21 @@
 
 import React from "react";
 import { signIn, useSession } from "next-auth/react";
+import  toast  from 'react-hot-toast';
+
 
 const loginPage = () => {
+  
   return (
     <div className="flex flex-col justify-center items-center gap-5">
       Login Page
       <button 
       type="button" 
       className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-      onClick={() => signIn("github")}>
+      onClick={() => {
+        console.log('Logged with GitHub')
+        toast.success('Logged with GitHub');
+        signIn("github")}}>
         GitHub login
       </button>
       
